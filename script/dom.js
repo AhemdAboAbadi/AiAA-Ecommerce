642fdf7f511008dabdf94d51e1b32d6ff213440e
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-07-14 11:07:37 +0300

    creat a functions make a home list or grid Relates #7

--- script/dom.js
+++ script/dom.js
@@ -30,7 +30,7 @@ function creatCart(item) {
 }
 
 function creatCartList(arr) {
-  const cartList = document.createElement("div");
+  const cartList = document.createElement("section"); //was a div
   cartList.classList.add("main-page");
   arr.forEach(function (item) {
     const cart = creatCart(item);
@@ -39,22 +39,60 @@ function creatCartList(arr) {
   return cartList;
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+
+function creatCartAsList(item) {
+  const cartMainContainerList = document.createElement("div");
+  const cartContainerList = document.createElement("div");
+  const cartImgList = document.createElement("img");
+  const cartTagList = document.createElement("h4");
+  const cartPriceList = document.createElement("span");
+  const cartDescriptionTitleList = document.createElement("p");
+  const cartDescriptionList = document.createElement("p");
+
+  cartMainContainerList.classList.add("main-page-list");
+  cartContainerList.classList.add("div-main-list");
+  cartContainerList.id = item.id;
+  cartImgList.classList.add("main-img-list");
+  cartTagList.classList.add("main-h4-list");
+  cartPriceList.classList.add("main-price-list");
+  cartDescriptionTitleList.classList.add("main-description-list");
+  cartDescriptionList.classList.add("main-details-list");
+
+  cartTagList.appendChild(document.createTextNode(item.category));
+  cartDescriptionTitleList.appendChild(document.createTextNode("Description"));
+  cartImgList.setAttribute("src", item.imgUrl);
+  cartDescriptionList.appendChild(document.createTextNode(item.description));
+  cartPriceList.appendChild(document.createTextNode(item.price));
+
+  cartContainerList.appendChild(cartTagList);
+  cartContainerList.appendChild(cartPriceList);
+  cartContainerList.appendChild(cartDescriptionTitleList);
+  cartContainerList.appendChild(cartDescriptionList);
+
+  cartMainContainerList.appendChild(cartImgList);
+  cartMainContainerList.appendChild(cartContainerList);
+
+  return cartMainContainerList;
+}
+
+function creatCartListAsList(arr) {
+  const sectionContainerList = document.createElement("section");
+  sectionContainerList.classList.add("container-cart");
+  arr.forEach((item) => {
+    const cart = creatCartAsList(item);
+    sectionContainerList.appendChild(cart);
+  });
+  return sectionContainerList;
+}
+
 function creatHome() {
   const main = document.querySelector("main");
   while (main.firstChild) {
     main.removeChild(main.lastChild);
   }
-  const cartList = creatCartList([
-    {
-      id: 0,
-      name: "fsd",
-      price: 1000,
-      category: "Tech",
-      description:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis odio animi a, illum sapiente magnam? Incidunt quisquam illo provident, sunt labore nemo sit non cupiditate consequuntur, optio debitis tempore et.",
-      imgUrl: "https://www.notebookcheck.net/uploads/tx_nbc2/SL4_AMD_1.jpg",
-    },
-  ]);
+  const cartList = creatCartList(getItem("products"));
+  // const cartList = creatCartListAsList(getItem("products"));
 
   main.appendChild(cartList);
 }
